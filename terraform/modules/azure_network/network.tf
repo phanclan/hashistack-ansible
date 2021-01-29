02ce3f343aa2006fbef992c641020e7d969ae8bb
Peter Phan <pphan@hashicorp.com>
2021-01-28 16:16:34 -0800

    add terraform

--- terraform/modules/azure_network/network.tf
+++ terraform/modules/azure_network/network.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.network.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
 resource "azurerm_subnet" "subnet" {
